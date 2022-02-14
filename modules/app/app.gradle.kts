1530d19289cf636430d15fa1737536b0f9781db6
triplem <160079+triplem@users.noreply.github.com>
2022-02-14 21:39:13 +0100

    chore: force failure

--- modules/app/app.gradle.kts
+++ modules/app/app.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 dependencies {
     implementation("org.apache.commons:commons-text")
     implementation(project(":utilities"))
+
+    implementation("org.apache.logging.log4j:log4j-core:2.16.0")
 }
 
 application {
