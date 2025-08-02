dc849bd56a24543dc56dc061246c9769114de41c
triplem <160079+triplem@users.noreply.github.com>
2025-08-02 12:28:21 +0200

    fix: remove asciidoctor warnings concerning startup parameters

--- buildSrc/src/main/kotlin/org/javafreedom/documentation/asciidoc-producer-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/org/javafreedom/documentation/asciidoc-producer-conventions.gradle.kts
@@ -18,6 +18,9 @@ val revNumber = System.getenv()["revnumber"] ?: "DEV-Version"
 val asciidoctorTask = tasks.named<AsciidoctorTask>("asciidoctor") {
     setSourceDir(file("docs"))
     setOutputDir(layout.buildDirectory.dir("docs"))
+    
+    // Asciidoctor plugin is not yet compatible with configuration cache
+    notCompatibleWithConfigurationCache("Asciidoctor plugin uses deprecated StartParameter API")
 
     resources {
         from("docs/resources") {
